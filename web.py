ca42a9839edd6c358b2d549a83d57ec6e8d8d8be
alexroz555 <114452472+alexroz555@users.noreply.github.com>
2024-02-17 17:21:05 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import requests
 import re
 from datetime import datetime
 app = Flask(__name__)
+website_fetched = False
 
 @app.route('/')
 def index():
     date = request.args.get('date')
-    open_website(date)
+    #open_website(date)
     return render_template('index.html')
 
 def convert_date(date_str):
@@ -17,52 +17,60 @@ def convert_date(date_str):
     formatted_date = date_item.strftime('%d %B %Y')
     return formatted_date 
 
-
 @app.route('/tide_form', methods=['POST'])
 def tide_form():
     #process form data
     location = request.form['location']
     date = request.form['date']
-    formatted_date = convert_date(date)
-    #time = request.form['time']
-    formatted_high, formatted_low = open_website(formatted_date)
-
-    return redirect(url_for('new_page', location=location, date=formatted_date, formatted_high=formatted_high, formatted_low=formatted_low))
+    #so site doesn't crash if no date is entered
+    if date == '':
+        return render_template('index.html')
+    else:
+        formatted_date = convert_date(date)
+        #time = request.form['time']
+        #formatted_high, formatted_low = open_website(formatted_date)
+        #return redirect(url_for('new_page'))
+        return redirect(url_for('new_page', location=location, date=formatted_date))
 
 @app.route('/new_page')
 def new_page():
     location = request.args.get('location')
     date = request.args.get('date')
-    formatted_high = request.args.get('formatted_high')
-    formatted_low = request.args.get('formatted_low')
+    formatted_high = ""
+    formatted_low = ""
+    if website_fetched:
+        formatted_high, formatted_low = process_data(date)
     return render_template('new_page.html', location=location, date=date, formatted_high=formatted_high, formatted_low=formatted_low)
 
 @app.route("/return_home", methods=['GET'])
 def return_home():
     return redirect(url_for('index'))
 
-def open_website(date):
+def open_website():
+    global website_fetched
+
+    url = "https://www.tide-forecast.com/locations/Newport-Yaquina-River-Oregon/tides/latest"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    formatted_text = BeautifulSoup(text, "html.parser").prettify()
+
+    with open("output.txt","w") as file:
+        file.write(formatted_text)
+
+    website_fetched = True
+
+def process_data(date):
     high_data = {}
     low_data = {}
 
     formatted_high = ""
     formatted_low = ""
 
-
-    url = "https://www.tide-forecast.com/locations/Newport-Yaquina-River-Oregon/tides/latest"
-    page = urlopen(url)
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-    
-    soup = BeautifulSoup(html, "html.parser")
-
-    text = soup.get_text()
-    formatted_text = BeautifulSoup(text, "html.parser").prettify()
-    with open("output.txt","w") as file:
-        file.write(formatted_text)
-    
     with open("output.txt", "r") as file:
-        print_flag = False
         lines = file.read()
         target = date
         start_index = lines.find(target)
@@ -96,7 +104,7 @@ def open_website(date):
         low_data[tide_type].append((tide_time, tide_height))
 
     for tide_type, tide_data in high_data.items():
-        formatted_high += f"{tide_type} Tide: "
+        #formatted_high += f"{tide_type} Tide: "
         for i, (time, height) in enumerate(tide_data):
             formatted_high += f"{time}, {height}"
             if i < len(tide_data) - 1:
@@ -104,7 +112,7 @@ def open_website(date):
         formatted_high += "; "
 
     for tide_type, tide_data in low_data.items():
-        formatted_low += f"{tide_type} Tide: "
+        #formatted_low += f"{tide_type} Tide: "
         for i, (time, height) in enumerate(tide_data):
             formatted_low += f"{time}, {height}"
             if i < len(tide_data) - 1:
@@ -114,4 +122,5 @@ def open_website(date):
     return formatted_high, formatted_low
 
 if __name__ == '__main__':
+    open_website()
     app.run(debug=True)
